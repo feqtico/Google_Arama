1179b945cba9efa970650aeb9a3979c40b2784cd
feqtico <73079578+feqtico@users.noreply.github.com>
2021-06-13 13:11:53 -0400

    Add files via upload

---
+++ google_arama.py
@@ -0,0 +1,12 @@
+
+from googlesearch import search
+
+
+print("Google Arama Programına Hoş Geldiniz.")
+  
+word = input(" Aranacak Kelime Girin: ")
+
+arama = word
+  
+for j in search(arama, tld="com", num=10, stop=10, pause=2):
+    print(j)
